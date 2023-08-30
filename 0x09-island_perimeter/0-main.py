10f00a1d902a90b676411d3f325216df18549e94
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-08-30 17:19:29 +0100

    Update 0-main.py

--- 0x09-island_perimeter/0-main.py
+++ 0x09-island_perimeter/0-main.py
@@ -1,1 +1,15 @@
-0x09-island_perimeter
+#!/usr/bin/python3
+"""
+0-main
+"""
+island_perimeter = __import__('0-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
